fd731b7c27438d39c3d3edd60db31f775ac9408c
the-dsw <fmod@the-dsw.com>
2017-07-31 23:36:07 +0200

    app.js updated

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ var express = require("express")
 var app = express()
 var useragent = require("express-useragent")
 const PORT = 3000
+var api = '/api/whoami'
 
 app.use(useragent.express())
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
+  res.redirect(api);
+});
+
+app.get(api, function(req, res) {
   var ip = req.ip;
   var language = req.acceptsLanguages()
-  var software = "OS: " + req.useragent.os + ", Browser: " + req.useragent.browser
+  var software = "os: " + req.useragent.os + ", browser: " + req.useragent.browser
 
   res.json({
     'ipaddress': ip,
     'language': language,
     'software': software
   });
+
 
 })
 
